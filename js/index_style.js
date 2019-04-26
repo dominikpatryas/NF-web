64e6b58995bf72fb944b5d76e90189b5c66ed634
Dominik Patryas <dominik.patryas@gmail.com>
2019-04-26 12:10:34 +0200

    added jquery effects

--- js/index_style.js
+++ js/index_style.js
@@ -1,12 +1,35 @@
-$(function(){
-    function yourfunction(event) {
-      if (document.getElementById('low-res-div').style.display == "block") {
-        document.getElementById('low-res-div').style.display = "none";
-      } else {
-        document.getElementById('low-res-div').style.display = "block";
-      }
+// $(function(){
+//     function yourfunction(event) {
+//       if (document.getElementById('low-res-div').style.display == "block") {
+//         document.getElementById('low-res-div').style.display = "none";
+//       } else {
+//         document.getElementById('low-res-div').style.display = "block";
+//       }
       
+//     }
+//     $('.fa-align-justify').click(yourfunction);
+// });
+
+$(document).ready(function(){
+  $(".fa-align-justify").click(function(){
+
+    if($('#low-res-div').css('display') == 'none')
+    {
+      $("#low-res-div").fadeIn("slow");
     }
-    $('.fa-align-justify').click(yourfunction);
+else {
+  $("#low-res-div").fadeOut();
+}
+  });
+});
+
+$(document).ready(function () {
+  $('body').css('display', 'none').fadeIn(1000);
+  $('.first').css('display', 'none').slideDown(1500);
+  $('.opis p').css('display', 'none').fadeIn(1500);
+  $('.second').css('display', 'none').fadeIn(2500);
+
+ 
+
 });
 
